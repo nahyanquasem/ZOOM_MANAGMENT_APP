593b5fc02aafab9907e4b5dd4c186facde3cd1fc
Nahyan Quasem <nahyan.quasem@gmail.com>
2025-12-15 21:36:04 +0600

    Import, setup and test of httpx library

--- src/zoom_async_api_calls.py
+++ src/zoom_async_api_calls.py
@@ -1,1 +1,15 @@
 #asynchronous Zoom api calls with batch runner functions
+
+import httpx
+import asyncio
+
+
+if __name__ == '__main__':
+
+    url = 'https://jsonplaceholder.typicode.com/posts/1'
+
+    with httpx.Client() as client:
+        response = client.get(url).json()
+
+    for key, values in response.items():
+        print(f'{key} : {values}')
