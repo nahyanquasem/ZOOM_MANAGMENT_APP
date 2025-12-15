15aff570a5e0404f7277b7e2fdf6367c87c56f68
Nahyan Quasem <nahyan.quasem@gmail.com>
2025-12-15 21:32:08 +0600

    Import, setup and synchronous requests library

--- src/zoom_api_calls.py
+++ src/zoom_api_calls.py
@@ -1,1 +1,10 @@
 #synchronous zoom api calls
+
+import requests
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/posts/1'
+    response = requests.get(url).json()
+
+    for key, values in response.items():
+        print(f'{key} : {values}')
