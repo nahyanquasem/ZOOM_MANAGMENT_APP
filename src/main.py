30493391963493078b61636c00b42e18703f38da
Nahyan Quasem <nahyan.quasem@gmail.com>
2025-12-16 17:40:22 +0600

    Loaded keys and tested to be working

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from dotenv import load_dotenv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    load_dotenv()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(f'{os.getenv('zoom_oauth_endpoint')}')
+    print(f'{os.getenv('client_secret')}')
