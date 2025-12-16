70e3ca807dedebf8fd02f8efe122f877400c4c7c
Nahyan Quasem <nahyan.quasem@gmail.com>
2025-12-16 09:01:51 +0600

    Testing httpx connection pooling with multiple concurrent async requests

--- src/zoom_async_api_calls.py
+++ src/zoom_async_api_calls.py
@@ -3,13 +3,42 @@
 import httpx
 import asyncio
 
+async def fetch(url:str, client: httpx.AsyncClient):
+
+    response = await client.get(url)
+    return response
+
+
+async def fetch_batch_urls(urls: list[str]):
+
+    async with httpx.AsyncClient() as client:
+        task = [asyncio.create_task(fetch(url,client)) for url in urls]
+        response = await asyncio.gather(*task)
+
+    return response
+
+
+
+
+def main():
+
+    url = 'https://webhook.site/73f56245-f9a2-4e33-b5b7-73b9f998cf58'
+    urls = [url for _ in range(0,10)]
+
+    responses = asyncio.run(fetch_batch_urls(urls))
+
+    for each_response in responses:
+        print(each_response)
+
+    # url = 'https://jsonplaceholder.typicode.com/posts/1'
+    #
+    # with httpx.Client() as client:
+    #     response = client.get(url).json()
+    #
+    # for key, values in response.items():
+    #     print(f'{key} : {values}')
+
+
 
 if __name__ == '__main__':
-
-    url = 'https://jsonplaceholder.typicode.com/posts/1'
-
-    with httpx.Client() as client:
-        response = client.get(url).json()
-
-    for key, values in response.items():
-        print(f'{key} : {values}')
+    main()
